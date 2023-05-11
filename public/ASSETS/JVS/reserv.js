99c0bb51665af16b9ad5ad69b2edd3e81a349dd9
Jean-Marc BOUTAUD <jmbstudi@gmail.com>
2023-05-11 12:06:13 +0200

    Code cleaned on booking twig & JS

--- public/ASSETS/JVS/reserv.js
+++ public/ASSETS/JVS/reserv.js
@@ -1,56 +1,59 @@
 console.log('réservations, bonjour')
 
+const myData = document.getElementById('shareData')
 const bookDate = document.querySelector("#book-date")
 const bookHour = document.querySelector("#reservHour")
-const btonsQr = document.querySelectorAll(".btonQuarter")
+const bookSeat = document.querySelector("#seats")
+const btonsQrs = document.querySelectorAll(".btonQuarter")
 
 let remainSeats
 let weekHours
 let day = {}
+let nbSeatsReserved 
 
 
-
-//----- GET DATA COMING FROM REPO -> CONTROLLER -> TWIG
+//----- GET DATA COMING FROM <- TWIG <- CONTROLLER <- REPO
 document.addEventListener('DOMContentLoaded', function() {
-  const myData = document.getElementById('shareData')
+ 
+  // opening hours of the week
   weekHours = JSON.parse(myData.dataset.week)
+
+  // remaining nb of seats on next 3 weeks
   remainSeats = JSON.parse(myData.dataset.remainseats)
   
+  // initialize date Part for quarters pickers
   dateStart = bookDate.value
-  // console.log(weekHours[0].weekDay)
-  // console.log(remainSeats)
+  nbSeatsReserved = bookSeat.value
   updateHours(dateStart)
 
-  btonsQr.forEach(btonQr => {
+  btonsQrs.forEach(btonQr => {
     btonQr.addEventListener('click', (e) => {
         console.log('clic ' + e.target.value)
         bookHour.value = e.target.value
       })
   });
-
 })
 
-// selectQr.addEventListener('click', (e) => {
-//   console.log('clic')
-//   bookHour.textContent ="coucopui"
-// })
+// NB SEATS CHANGED? FOR NO SURBOOKING: UPDATE NB SEATS AVAILABLE
+bookSeat.addEventListener('change', (e) => {
+  console.log('seat'+e.target.value)
+  nbSeatsReserved = bookSeat.value
+  dateReserved = bookDate.value
+  updateHours(dateReserved)
+})
 
-
-
-
-// DATE IS CHANGED? MAKE UPDATE
+// DATE IS CHANGED? MAKE ALL QUARTER UPDATES
 bookDate.addEventListener('change', (e) => {
   dateNew = e.target.value
   updateHours(dateNew)
-  bookHour.value ='??:??'
 })
 
-
-// UPDATE HOURS
+// UPDATE QUARTER HOURS
 updateHours = (dateChoice) => {
   console.log("dateChoice = " + dateChoice);
+  bookHour.value ='??:??'
   
-  //look for the date
+  //look for the date in the file coming from Controller
   let nb = 0
   for(elt in remainSeats){
     if (remainSeats[elt].date === dateChoice){
@@ -67,22 +70,18 @@ updateHours = (dateChoice) => {
 
       console.log("day.dayNum = " + day.dayNum);
       console.log(weekHours)
-      // day.DinnerSeats = remainSeats[nb + 1].seats
-      // day.DinnerAvailable = remainSeats[nb + 1].available
       
       if (remainSeats[nb].mealTime === 'MIDI') {
-        startHour = new Date(weekHours[day.dayNum - 1].lunchStart)//.getTime()
-        endHour = new Date(weekHours[day.dayNum - 1].lunchEnd)//.getTime()
+        startHour = new Date(weekHours[day.dayNum - 1].lunchStart)
+        endHour = new Date(weekHours[day.dayNum - 1].lunchEnd)
         
       } else {
-        startHour = new Date(weekHours[day.dayNum - 1].dinnerStart)//.getTime()
-        endHour = new Date(weekHours[day.dayNum - 1].dinnerEnd)//.getTime()
+        startHour = new Date(weekHours[day.dayNum - 1].dinnerStart)
+        endHour = new Date(weekHours[day.dayNum - 1].dinnerEnd)
       }
 
     console.log (day)
 
-    // Q = nbQuarters(remainSeats[nb].mealTime, remainSeats[nb].dayNum)
-    
     hideQuarters(remainSeats[nb].mealTime)
 
     showQuarters(
@@ -90,89 +89,67 @@ updateHours = (dateChoice) => {
       startHour,
       endHour,
       remainSeats[nb].seats != 0,
-      remainSeats[nb].available > 0
-      )
-      
+      remainSeats[nb].available - nbSeatsReserved > 0
+      )      
     }    
   }
 }
 
-  showQuarters = (meal, startHour, endHour, opened, availableSeats) => {
-    if (!opened) {
-      console.log(meal + ' fermé !')
-      document.getElementById(meal+"closed").style.display = "block"
-      document.getElementById(meal+"full").style.display = "none"
-      document.getElementById(meal+"quarters").style.display = "none"
+showQuarters = (meal, startHour, endHour, opened, availableSeats) => {
+  if (!opened) {
+    // RESTAURANT IS CLOSED
+    document.getElementById(meal+"closed").style.display = "block"
+    document.getElementById(meal+"full").style.display = "none"
+    document.getElementById(meal+"quarters").style.display = "none"
+  } 
 
-    } else {
+  else if (!availableSeats) {
+    // RESTAURANT HAS NO ROOM AVAILABLE VS THE RESERVATION DEMAND
+    document.getElementById(meal+"closed").style.display = "none"
+    document.getElementById(meal+"full").style.display = "block"
+    document.getElementById(meal+"quarters").style.display = "none"
+  } 
+    
+  else {
+    // RESTAURANT IS OPENED
+    document.getElementById(meal+"closed").style.display = "none"
+    document.getElementById(meal+"full").style.display = "none"
+    document.getElementById(meal+"quarters").style.display = "block"
+  
+    // SHOW THE QUARTERS CORRESPONDING TO THE WEEK'S DAY
 
-      if (availableSeats){
-        console.log(meal + ' ouvert !')
-        document.getElementById(meal+"closed").style.display = "none"
-        document.getElementById(meal+"full").style.display = "none"
-        document.getElementById(meal+"quarters").style.display = "block"
-      } else {
-        console.log(meal + ' full !')
-        document.getElementById(meal+"closed").style.display = "none"
-        document.getElementById(meal+"full").style.display = "block"
-        document.getElementById(meal+"quarters").style.display = "none"
+    // bookings stop 1 hour before Restaurant closing
+    endHour.setHours (endHour.getHours() - 1)
+    console.log("endHour = " + endHour);
+    
+    //show and update the quarters text (19:00, 19:15...)
+    cpt = 1
+    do {
+      bton = document.getElementById(meal+cpt)
+      bton.value = showHour(startHour)
+      bton.style.display = "block"
 
-      }
+      startHour.setMinutes( startHour.getMinutes() + 15) 
+      cpt++
 
-      cpt = 1
-      // showHour = startHour
-      endHour.setHours (endHour.getHours() - 1)
-      // console.log("showHour = " + showHour);
-      console.log("endHour = " + endHour);
-      
-      //update the quarters choice
-      do {
-        // console.log("meal+cpt = " + meal+cpt);
-        // showHour = startHour.getHours() + ":" + startHour.getMinutes()
-        bton = document.getElementById(meal+cpt)
-        // bton.textContent = showHour(startHour)
-        bton.value = showHour(startHour)
-        bton.style.display = "block"
-
-        startHour.setMinutes( startHour.getMinutes() + 15) 
-        // console.log("new showHour = " + startHour);
-        cpt++
-
-      } while (startHour <= endHour);
-
-    }
+    } while (startHour <= endHour)
   }
+}
 
 // format Hour for display on buttons
-  showHour = (myHour) =>{
-    hh = myHour.getHours()
-    mm = myHour.getMinutes()
-    H = hh < 10 ? '0' + hh : '' + hh
-    M = mm < 10 ? '0' + mm : '' + mm
-    return (H +':' + M)
-  }
+showHour = (myHour) =>{
+  hh = myHour.getHours()
+  mm = myHour.getMinutes()
+  H = hh < 10 ? '0' + hh : '' + hh
+  M = mm < 10 ? '0' + mm : '' + mm
+  return (H +':' + M)
+}
 
-  // calculate how many quarters of an hour we have to propose:
-  nbQuarters = (meal, dayNum) =>{
-    console.log("dayNum = " + dayNum);
-    Q = {}
-
-
-
-    // Q.nbQlunch = Math.round((Q.endHour - Q.startHour) /15/60/1000)
-    // // substract 1 hour for the service:
-    // Q.nbQlunch -= 4
-    
-    // console.log("nbQlunch = " + Q.nbQlunch);    
-    return Q
-  }
-  
+// HIDE BUTTONS OUT OF RESERVATION TIME
 hideQuarters = (meal) => {
-
   for (let i = 1; i <= 18; i++) {
 
     quarter = document.getElementById(meal + i)
-    console.log("quarter = " + quarter.style.display);
     if (quarter.style.display != "none"){
       quarter.style.display = "none"
     } else {
